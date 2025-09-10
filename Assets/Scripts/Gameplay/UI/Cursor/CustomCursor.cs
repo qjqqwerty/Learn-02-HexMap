0b3ec697d36e7186b1f5227a7c389595e2fb558a
qjqqwerty <qjqqwerty@sina.com>
2025-09-10 17:54:01 +0800

    优化光标高亮UI Button 的效果

--- Assets/Scripts/Gameplay/UI/Cursor/CustomCursor.cs
+++ Assets/Scripts/Gameplay/UI/Cursor/CustomCursor.cs
@@ -12,6 +12,12 @@ public class CustomCursor : MonoBehaviour
     private void Awake()
     {
         _skinImage = GetComponentInChildren<Image>();
+        Cursor.visible = false;  // 一开始就隐藏系统光标
+
+        if (_skinImage != null)
+        {
+            _skinImage.raycastTarget = false; // 避免拦截 UI 事件
+        }
     }
 
     private void Update()
@@ -21,7 +27,6 @@ public class CustomCursor : MonoBehaviour
         // 鼠标按下切换贴图
         if (Input.GetMouseButtonDown(0))
         {
-            Cursor.visible = false;
             _skinImage.sprite = 按下光标;
         }
         if (Input.GetMouseButtonUp(0))
@@ -29,7 +34,7 @@ public class CustomCursor : MonoBehaviour
             _skinImage.sprite = 普通光标;
         }
 
-        // 光标跟随鼠标位置（UI 用屏幕坐标）
+        // 光标跟随鼠标
         transform.position = Input.mousePosition;
     }
 
