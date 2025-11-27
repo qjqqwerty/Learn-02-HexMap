7cc099a845fd47b75451fcaf2d2f4af62a716ffc
qjqqwerty <qjqqwerty@sina.com>
2025-11-27 08:03:34 +0800

    新增 ScreenUI.cs 脚本   新增 用于控制界面开关

--- Assets/Scripts/Gameplay/UI/Screen/ScreenUI.cs
+++ Assets/Scripts/Gameplay/UI/Screen/ScreenUI.cs
@@ -6,15 +6,19 @@ public class ScreenUI : MonoBehaviour
 {
     [Tooltip("要控制的目标对象")]
     public GameObject target; 
-    private CanvasGroup canvasGroup;
-    void Start()
+    [Tooltip("按下这个键时切换显示/隐藏")]
+    public KeyCode toggleKey = KeyCode.X;   // 默认 X，可在 Inspector 修改
+    private void Awake()
     {
-        
+        target = target.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(toggleKey))
+        {
+            target.SetActive(!target.activeSelf);
+        }
     }
 }
